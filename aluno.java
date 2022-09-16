d0470b6cccc477812231f07eb7b48e2b934b7e51
Alessandro <viniciusmachado1106@gmail.com>
2022-09-16 11:22:49 -0300

    Atividade Alex

---
+++ aluno.java
@@ -0,0 +1,17 @@
+package boletim;
+
+//Vinicius 3-52
+public class aluno {
+	String nomeAluno, disciplina;
+	float n1, n2, n3, n4, media;
+	
+	void disciplina() {
+		
+	}
+	
+	
+	void notaAluno() {
+
+	}
+	
+}
---
+++ professor.java
@@ -0,0 +1,5 @@
+package boletim;
+//Vinicius 3-52
+public class professor {
+	String nome, disciplina;
+}
